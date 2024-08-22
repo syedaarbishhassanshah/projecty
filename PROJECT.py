8eb59f9c23e5cf5e9e43597fe4dd09774be5e466
SYEDA ARBISH <jugnoo708@gmail.com>
2024-08-22 12:42:21 +0500

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,9 @@
+from tkinter import*
+from tkinter import Tk
+
+root = Tk()
+root.geometry('500x400')
+root.title('TRANSLATOR')
+root.resizeable('false,false')
+root.configure(bg='purple')
+root.mainloop 
